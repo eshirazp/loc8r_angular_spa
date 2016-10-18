8c60f5030329d748a6f5a4ab1c0022a483aa98fc
Elush Shirazpour <eshirazp@gmail.com>
2016-10-18 00:36:04 -0700

    Setting routing tasks to Angular

---
+++ app_client/app.js
@@ -0,0 +1,13 @@
+angular.module('loc8rApp', ['ngRoute']);
+
+function config ($routeProvider) {
+  $routeProvider
+    .when('/', {
+
+    })
+    .otherwise({redirectTo: '/'});
+}
+
+angular
+  .module('loc8rApp')
+  .config(['$routeProvider', config]);
